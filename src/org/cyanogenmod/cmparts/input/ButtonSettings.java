b0045f674503fb7bf663ba87fc80f653d0f709f8
LuK1337 <priv.luk@gmail.com>
2018-01-08 15:05:56 +0100

    CMParts: Don't disable whole home button category
    * Most of these settings still work despite
      having software navigation bar enabled.
    
    Change-Id: I13760438a1a7e6690c90c119b710408aa5f39d6c
    
    Conflicts:
    	src/org/cyanogenmod/cmparts/input/ButtonSettings.java

--- src/org/cyanogenmod/cmparts/input/ButtonSettings.java
+++ src/org/cyanogenmod/cmparts/input/ButtonSettings.java
@@ -246,13 +246,6 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
         mNavigationHomeDoubleTapAction = initActionList(KEY_NAVIGATION_HOME_DOUBLE_TAP,
                 homeDoubleTapAction);
 
-        // Hide navigation bar home settings if we have a hardware home key
-        // so that action config options aren't duplicated.
-        if (hasHomeKey) {
-                mNavigationPreferencesCat.removePreference(mNavigationHomeLongPressAction);
-                mNavigationPreferencesCat.removePreference(mNavigationHomeDoubleTapAction);
-        }
-
         // Navigation bar recents long press activity needs custom setup
         mNavigationRecentsLongPressAction =
                 initRecentsLongPressAction(KEY_NAVIGATION_RECENTS_LONG_PRESS);
@@ -303,8 +296,12 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
                 mHomeAnswerCall = null;
             }
 
-            mHomeLongPressAction = initActionList(KEY_HOME_LONG_PRESS, homeLongPressAction);
-            mHomeDoubleTapAction = initActionList(KEY_HOME_DOUBLE_TAP, homeDoubleTapAction);
+            mHomeLongPressAction = initList(KEY_HOME_LONG_PRESS, homeLongPressAction);
+            mHomeDoubleTapAction = initList(KEY_HOME_DOUBLE_TAP, homeDoubleTapAction);
+            if (mDisableNavigationKeys.isChecked()) {
+                mHomeLongPressAction.setEnabled(false);
+                mHomeDoubleTapAction.setEnabled(false);
+            }
 
             hasAnyBindableKey = true;
         } else {
@@ -675,8 +672,25 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
         }
 
         /* Toggle hardkey control availability depending on navbar state */
+        if (mNavigationPreferencesCat != null) {
+            if (navbarEnabled) {
+                mNavigationPreferencesCat.addPreference(mNavigationHomeLongPressAction);
+                mNavigationPreferencesCat.addPreference(mNavigationHomeDoubleTapAction);
+            } else {
+                mNavigationPreferencesCat.removePreference(mNavigationHomeLongPressAction);
+                mNavigationPreferencesCat.removePreference(mNavigationHomeDoubleTapAction);
+            }
+        }
         if (homeCategory != null) {
-            homeCategory.setEnabled(!navbarEnabled);
+            if (mHomeAnswerCall != null) {
+                mHomeAnswerCall.setEnabled(!navbarEnabled);
+            }
+            if (mHomeLongPressAction != null) {
+                mHomeLongPressAction.setEnabled(!navbarEnabled);
+            }
+            if (mHomeDoubleTapAction != null) {
+                mHomeDoubleTapAction.setEnabled(!navbarEnabled);
+            }
         }
         if (backCategory != null) {
             backCategory.setEnabled(!navbarEnabled);
